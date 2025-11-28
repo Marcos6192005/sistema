b88fd3d629337341a1c4cff9acf51386e9cd6ce0
Marcos6192005 <marcosvasquez619@gmail.com>
2025-11-28 09:01:28 -0600

    otros cambios

---
+++ logout.php
@@ -0,0 +1,23 @@
+<?php
+// Iniciamos la sesión
+session_start();
+
+// Destruir todas las variables de sesión
+$_SESSION = array();
+
+// Si existe una cookie de sesión, la eliminamos
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destruir la sesión
+session_destroy();
+
+// Redirigir al login
+header("Location: index.php");
+exit();
+?>
